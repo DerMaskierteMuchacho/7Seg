2adabc37ad5d578a20e178d9bfa503f74a90a7d7
Michel Mirsayyah <michel.mirsayyah@ost.ch>
2020-09-16 11:23:38 +0200

    added negative numbers

--- Calculator7Seg/AsciiGenerator.cpp
+++ Calculator7Seg/AsciiGenerator.cpp
@@ -11,10 +11,6 @@ std::string createStringFromArray(std::vector<std::vector<std::string>> array)
 		for (std::vector<std::string> level : array)
 		{
 			output += level[i] + spaceBetweenDigits;
-			if (i != 1 && i != 3)
-			{
-				output += " ";
-			}
 		}
 		output += "\n";
 	}
@@ -27,39 +23,48 @@ std::string AsciiGenerator::intToAsciiString(int input)
 	std::vector<std::vector<std::string>> output;
 	std::string inputAsString = std::to_string(input);
 
+	std::string empty = "    ";
+	std::string line = " -- ";
+	std::string bar = "|  |";
+	std::string barLeft = "|   ";
+	std::string barRight = "   |";
+
 	for (char digit : inputAsString)
 	{
 		switch (digit)
 		{
+		case '-':
+			output.push_back({ empty, empty, line, empty, empty });
+			break;
 		case '0':
-			output.push_back({ " --", "|  |", "   ", "|  |", " --" });
+			output.push_back({ line, bar, empty, bar, line });
 			break;
 		case '1':
-			output.push_back({ "   ", "   |", "   ", "   |", "   " });
+			output.push_back({ empty, barRight, empty, barRight, empty });
 			break;
 		case '2':
-			output.push_back({ " --", "   |", " --", "|   ", " --" });
+			output.push_back({ line, barRight, line, barLeft, line });
 			break;
 		case '3':
-			output.push_back({ " --", "   |", " --", "   |", " --" });
+			output.push_back({ line, barRight, line, barRight, line });
 			break;
 		case '4':
-			output.push_back({ "   ", "|  |", " --", "   |", "   " });
+			output.push_back({ empty, bar, line, barRight, empty });
 			break;
 		case '5':
-			output.push_back({ " --", "|   ", " --", "   |", " --" });
+			output.push_back({ line, barLeft, line, barRight, line });
 			break;
 		case '6':
-			output.push_back({ " --", "|   ", " --", "|  |", " --" });
+			output.push_back({ line, barLeft, line, bar, line });
 			break;
 		case '7':
-			output.push_back({ " --", "   |", "   ", "   |", "   " });
+			output.push_back({ line, barRight, empty, barRight, empty });
 			break;
 		case '8':
-			output.push_back({ " --", "|  |", " --", "|  |", " --" });
+			output.push_back({ line, bar, line, bar, line });
 			break;
 		case '9':
-			output.push_back({ " --", "|  |", " --", "   |", " --" });
+			output.push_back({ line, bar, line, barRight, line });
 			break;
 		}
 	}
